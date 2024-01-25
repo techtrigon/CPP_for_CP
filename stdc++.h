a9eb2929733232834d52a7e9ce060edf1088c949
Dev Vikram <78585046+techtrigon@users.noreply.github.com>
2024-01-25 16:44:48 +0530

    Add files via upload

--- stdc++.h
+++ stdc++.h
@@ -1,4 +1,33 @@
-#include <bits/stdcpp.h>
-using  namespace std;
-template<class K , class V>ostream& operator<<( ostream& s , const pair<K , V>& p ) { s << '<' << p.first << ", " << p.second << '>'; return s; }template<class T , class = typename T::value_type , class = typename enable_if<!is_same<T , string>::value>::type>ostream& operator<<( ostream& s , const T& v ) { s << "[ "; for( auto& x : v ) { s << x << ", "; }if( !v.empty ( ) ) { s << "\b\b"; }s << " ]"; return s; }void __prnt ( ) { cerr << "\n"; } template<class T , class...Ts>void __prnt ( T&& a , Ts&&...etc ) { cerr << a << ",  "; __prnt ( etc... ); }
-#define db(...) { cerr<<"... "<< #__VA_ARGS__<<" = ";__prnt(__VA_ARGS__); }
+// C
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+// #include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+// C++
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <set>
+#include <map>
+#include <list>
+#include <iterator>
+#include <string>
+#include <sstream>
+#include <functional>
+#include <forward_list>
+#include <random>
+#include <array>
+#include <chrono>
+#include <thread>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+// #include <stdexcept>
+// #include <type_traits>
